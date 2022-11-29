eb1d8ec8b869a8e5b7dcc4b7e366a94d887199b8
BT_3W2_03 <yanyeef@nycstudents.net>
2022-11-29 12:10:51 -0500

    added methods

--- src/Guess.java
+++ src/Guess.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Guess {
     private int randomNum;
     private int highScore;
+    private int length;
+    private int guess;
     private int pointAdd;
     private int pointMulti;
 
@@ -12,18 +14,41 @@ public class Guess {
         randomNum = (int)(Math.random()*10);
     }
 
-    public Guess(int length,int pointAdd,int pointMulti) {
+    public Guess(int length,int guess) {
         String tempFull = "";
         for (int i=1;i<=length;i++) {
             int tempDigit = (int)(Math.random()*10);
             tempFull += tempDigit;
         }
         randomNum = Integer.parseInt(tempFull);
-        this.pointAdd = pointAdd;
-        this.pointMulti = pointMulti;
+        this.length = length;
+        this.guess = guess;
+        pointAdd = 10-guess;
+        pointMulti = 1 + Math.abs(1-length);
     }
 
     public String toString() {
         return "" + randomNum;
+    }
+
+    public void printSettings(boolean hardMode) {
+        if (hardMode) {
+            System.out.println("----------------------");
+            System.out.println("| HARD MODE SETTINGS |");
+            System.out.println("----------------------");
+            System.out.println("Number length: " + length);
+            System.out.println("Guess amount: " + guess);
+            System.out.println("Point modifier: +" + pointAdd);
+            System.out.println("Point multiplier: x" + pointMulti);
+        }
+        else {
+            System.out.println("----------------------");
+            System.out.println("| EASY MODE SETTINGS |");
+            System.out.println("----------------------");
+            System.out.println("Number length: 1");
+            System.out.println("Guess amount: 10");
+            System.out.println("Point modifier: +0");
+            System.out.println("Point multiplier: x1");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,19 @@ public class Main {
         int highestScore;
         int pointAdd;
         int pointMulti;
+        boolean hardMode = false;
 
         System.out.println("Hello, " + name + "!");
         System.out.print("Would you like to play on HARD mode? Type \"Yes\" if so, otherwise press enter: ");
         String diff = input.nextLine();
         if (diff.toLowerCase().equals("yes")) {
-            System.out.print("Choose number of guesses (Max. 9; the lower the number of guesses, the more points you'll gain per number guessed correctly): ");
+            hardMode = true;
+            System.out.print("Choose number of guesses (Min. 1, Max. 9; the fewer guesses you have, the more points you'll gain per number guessed correctly): ");
             int guess = input.nextInt();
             while (guess<=0||guess>9) {
                 System.out.print("Error: Number of guesses must be between 1 and 9. Please reenter guesses: ");
                 guess = input.nextInt();
             }
-            pointAdd = 10-guess;
 
             System.out.print("Choose the length of the random number (Min. 2, Max. 7; any length over 3 will increase your score multiplier): ");
             int length = input.nextInt();
@@ -31,25 +32,13 @@ public class Main {
                 System.out.print("Error: Length must be between 2 and 7. Please reenter length: ");
                 length = input.nextInt();
             }
-            pointMulti = 1 + Math.abs(1-length);
-            Guess guesser = new Guess(length,pointAdd,pointMulti);
-            System.out.println("----------------------");
-            System.out.println("| HARD MODE SETTINGS |");
-            System.out.println("----------------------");
-            System.out.println("Number length: " + length);
-            System.out.println("Guess amount: " + guess);
-            System.out.println("Point modifier: +" + pointAdd);
-            System.out.println("Point multiplier: x" + pointMulti);
+
+            Guess guesser = new Guess(length,guess);
+            guesser.printSettings(hardMode);
         }
         else {
             Guess guesser = new Guess();
-            System.out.println("----------------------");
-            System.out.println("| EASY MODE SETTINGS |");
-            System.out.println("----------------------");
-            System.out.println("Number length: 1");
-            System.out.println("Guess amount: 10");
-            System.out.println("Point modifier: +0");
-            System.out.println("Point multiplier: x1");
+            guesser.printSettings(hardMode);
         }
     }
 }
